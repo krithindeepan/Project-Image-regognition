d924dfbe6c5b804fee5b216b196cd58d2b2a916e
krithindeepan <84080581+krithindeepan@users.noreply.github.com>
2022-04-22 20:00:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+    });
+    
+    camera = document.getElementById("camera");
+    
+    Webcam.attach( '#camera' );
+    
+    
+    
+    function take_snapshot() { Webcam.snap(function(data_uri) { document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'; }); }
+    console.log(ml5.version);
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/S-xFNRBGi/model.json",modeloaded);
+    function modeloaded(){
+        console.log("modelisloaded");
+    }
+    function check(){
+        img=document.getElementById("captured_image");
+        classifier.classify(img,gotresult)
+    }
+    function gotresult(error,results){
+    if(error)
+    {console.error(error);}
+    else
+    {
+    console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label;
+    document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+    }
